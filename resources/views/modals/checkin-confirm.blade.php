e276cb6d09723b0393f21661ff3f2beb1e04863c
Glukose1 <167117705+Glukose1@users.noreply.github.com>
2025-12-19 13:37:05 +0100

    fix translation name in modal

--- resources/views/modals/checkin-confirm.blade.php
+++ resources/views/modals/checkin-confirm.blade.php
@@ -4,7 +4,7 @@
         <div class="modal-content">
             <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                <h2 class="modal-title">{{ trans('general.checkin_confirm.title') }}</h2>
+                <h2 class="modal-title">{{ trans('general.checkin_confirm_title') }}</h2>
             </div>
             <div class="modal-body">
                 {!! nl2br(Helper::parseEscapedMarkedown($setting->checkin_confirm_text)) !!}
